e7b1f2454c540b3ada1948033eb92ff523b1f221
Flávio Henrique <56225731+F-programmer@users.noreply.github.com>
2020-07-31 18:29:52 -0300

    Aula 2 - Slider

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -3,10 +3,26 @@ import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria() {
-  return(
+  return (
     <>
       <PageDefault to='/' title='Voltar a Home'>
         <h1>Cadastro de Categoria</h1>
+
+        <form>
+
+          <label>
+            Nome da Categoria:
+          <input
+              type="text"
+            />
+          </label>
+
+          <button>
+            Cadastrar
+        </button>
+        </form>
+
+
         <Link to='/'>Ir para a Home</Link>
       </PageDefault>
     </>
